90e65deec955a3201bdf9db17169ce05b6707c7c
Javohir <javohir.iskandarov1010@gmail.com>
2025-08-24 01:31:38 +0500

    new file:   items_views.py

---
+++ items_views.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+
+router = APIRouter(
+    prefix='/items'
+)
+
+@router.get("")
+async def list_items():
+    return {"item1", "item2"}
+
+
+@router.get("/latest")
+async def latest_item():
+    return {"id": 0, "name": "latest"}
+
+
+@router.get("/{item_id}")
+async def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
+    return {"item": {"id": item_id}}
---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+import uvicorn
+from items_views import router as items_router
+
+app = FastAPI()
+
+app.include_router(items_router)
+
+class CreateUser(BaseModel):
+    email: EmailStr
+
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello world"}
+
+
+@app.get("/hello")
+async def hello(name: str = "World"):
+    name = name.strip().capitalize()
+    return {"message": f"Hello {name}"}
+
+
+@app.post("/email")
+async def email_validator(user: CreateUser):
+    return {"message": "OK", "email": user.email}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
