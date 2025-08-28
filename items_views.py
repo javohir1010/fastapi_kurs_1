6c28979e8e6f87e733726ed131e26b01c8dec340
Javohir <javohir.iskandarov1010@gmail.com>
2025-08-28 09:57:06 +0500

    незначительные изменения

--- items_views.py
+++ items_views.py
@@ -1,19 +1,19 @@
 from typing import Annotated
 from fastapi import APIRouter, Path
 
-router = APIRouter(prefix="/items", tags=["Предметы"])
+router = APIRouter(prefix="/items", tags=["Items"])
 
 
-@router.get("")
+@router.get("/")
 async def list_items():
     return {"item1", "item2"}
 
 
-@router.get("/latest")
+@router.get("/latest/")
 async def latest_item():
     return {"id": 0, "name": "latest"}
 
 
-@router.get("/{item_id}")
+@router.get("/{item_id}/")
 async def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
     return {"item": {"id": item_id}}
